959b9e7c0512efa2b7c3acfd484f9d7a00d5c316
Luciene <lucienebsousa@gmail.com>
2024-12-13 15:17:52 -0300

    Login com sucesso e validação

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,17 @@
 describe('login', () => {
   it('Deve logar com sucesso', () => {
+    //cy.viewport(1920, 1080)
     cy.visit('https://playground.cyskills.com.br')
+
+    //preenchimento do formulário
+    cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
+    cy.get('[data-cy="password"]').type('showtime')
+
+    cy.get('[data-cy="login-button"]').click()
+
+    //validação do login
+    cy.get('[data-cy="welcome-title"]').should('be.visible').and('have.text', 'Boas vindas ao Cypress Playground')
+
+
   })
 })
